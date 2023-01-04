8acbf24da93fc1ed56bed01a1de253843d76856e
VitalikMelnyk <melnyk.vitalikk@gmail.com>
2023-01-04 18:11:56 +0200

    feat: add nodemailer

--- config/plugins.js
+++ config/plugins.js
@@ -6,5 +6,22 @@ module.exports = ({ env }) => ({
   seo: {
     enabled: true,
   },
-  //...
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.gmail.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: "mihael.melnyk@gmail.com",
+          pass: "lohpiyavtgntkqeg",
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: "mihael.melnyk@gmail.com",
+        defaultReplyTo: "mihael.melnyk@gmail.com",
+      },
+    },
+  },
 });
