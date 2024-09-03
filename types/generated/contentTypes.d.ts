4d69c1d34111d88b46429631486810fe6e4be240
VitalikMelnyk <vitalii.melnyk@underarmour.com>
2024-09-03 11:13:49 +0300

    fix: change teamName enumaration

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -876,7 +876,9 @@ export interface ApiProductProduct extends Schema.CollectionType {
       'manyToOne',
       'plugin::users-permissions.user'
     >;
-    teamName: Attribute.Enumeration<['team-1', 'team-2', 'team-3']> &
+    teamName: Attribute.Enumeration<
+      ['team-1', 'team-2', 'team-3', 'team-4', 'team-5']
+    > &
       Attribute.Required;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
